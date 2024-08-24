d2e5f322fc0c35e76d1ae90c9b17b3361424bd0a
nas <nastaran.bapir@gmail.com>
2024-08-25 02:59:14 +0330

    Upload DownloadFile project on Github

---
+++ DownloadFile/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace DownloadFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.txt_savedin = new System.Windows.Forms.TextBox();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.btn_download = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(627, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "لینک";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(622, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ذخیره در";
+            // 
+            // txt_url
+            // 
+            this.txt_url.Location = new System.Drawing.Point(30, 42);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(568, 20);
+            this.txt_url.TabIndex = 2;
+            // 
+            // txt_savedin
+            // 
+            this.txt_savedin.Location = new System.Drawing.Point(122, 86);
+            this.txt_savedin.Name = "txt_savedin";
+            this.txt_savedin.ReadOnly = true;
+            this.txt_savedin.Size = new System.Drawing.Size(476, 20);
+            this.txt_savedin.TabIndex = 3;
+            // 
+            // btn_select
+            // 
+            this.btn_select.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btn_select.Location = new System.Drawing.Point(30, 84);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(86, 32);
+            this.btn_select.TabIndex = 4;
+            this.btn_select.Text = "انتخاب";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // btn_download
+            // 
+            this.btn_download.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btn_download.Location = new System.Drawing.Point(512, 121);
+            this.btn_download.Name = "btn_download";
+            this.btn_download.Size = new System.Drawing.Size(86, 32);
+            this.btn_download.TabIndex = 5;
+            this.btn_download.Text = "دانلود فایل";
+            this.btn_download.UseVisualStyleBackColor = true;
+            this.btn_download.Click += new System.EventHandler(this.btn_download_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(30, 180);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(568, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 215);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btn_download);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.txt_savedin);
+            this.Controls.Add(this.txt_url);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "فرم دانلود";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.TextBox txt_savedin;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.Button btn_download;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ DownloadFile/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace DownloadFile
+{
+    public partial class Form1 : Form
+    {
+        WebClient wc = new WebClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            Random rnd = new Random();
+            string aa=Path.GetExtension(txt_url.Text);
+            if (folderBrowserDialog1.SelectedPath != "")
+                txt_savedin.Text = folderBrowserDialog1.SelectedPath + "\\Download" + rnd.Next(1000, 9999) +aa;
+        }
+
+        private void btn_download_Click(object sender, EventArgs e)
+        {
+            wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(wc_DownloadProgressChanged);
+            wc.DownloadFileCompleted += new AsyncCompletedEventHandler(wc_DownloadFileCompleted);
+            if (txt_url.TextLength == 0 || txt_savedin.TextLength == 0)
+                MessageBox.Show("تکست باکس ها خالی میباشند");
+            else
+            {
+                Uri imaguri = new Uri(txt_url.Text);
+                wc.DownloadFileAsync(imaguri, txt_savedin.Text);
+            }
+            
+        }
+        private void wc_DownloadProgressChanged(object sender,DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+        private void wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("فایل کامل دانلود شد","دانلود فایل",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+    }
+}
